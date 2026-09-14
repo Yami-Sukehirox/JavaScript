987d81c2e516efcd40b59c9dcc3d2883725c18c1
unknown <liancruspero1@gmail.com>
2026-09-14 22:12:27 +0800

    New

--- code.js
+++ code.js
@@ -13,26 +13,28 @@ console.log('====STUDENT PROFILE====');
 console.log('Name: ' + student.name + ' ' +student.lastName);
 console.log('Course: ' + student.course);
 
-if (student.course === 'BSIT' || 'BSCS' ){
+if (student.course === 'BSIT' || student.course === 'BSCS' ){
     console.log('IT-related student');
 }
 
-if (student.yearLevel === 1) {
-    console.log('Level: First Year');
-} else if (student.yearLevel === 2) {
-    console.log('Level: Second Year');
-} else if (student.yearLevel === 3) {
-    console.log('Level: Third Year');
+if (student.course === 'BSIT' && student.yearLevel === 1) {
+    console.log('Level: First Year BSIT STUDENT');
+} else if (student.course === 'BSIT' && student.yearLevel === 2) {
+    console.log('Level: Second Year BSIT STUDENT');
+    } else if (student.course === 'BSIT' && student.yearLevel === 3) {
+        console.log('Level: Third Year BSIT STUDENT');
 } else {
-    console.log('Level: Fourth Year');
+    console.log('Level: Fourth Year BSIT STUDENT');
 }
     
-
-
 if (student.lovesCoding === true) {
     console.log('Loves Coding: Yes!');
 } else {
     console.log('Loves Coding: No!');
+}
+
+if (student.course === 'BSIT' && student.lovesCoding === true) {
+    console.log('Interested in IT and Coding');
 }
 
 console.log('Programming Languages:' );
@@ -41,4 +43,5 @@ console.log(programmingLanguages[0]);
 console.log(programmingLanguages[1]);
 console.log(programmingLanguages[2]);
 }
+
 showStudentInfo();
