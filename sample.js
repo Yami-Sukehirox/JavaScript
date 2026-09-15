cc0ce01f38e84cb65da8207f82d189ca6fa9b785
unknown <liancruspero1@gmail.com>
2026-09-16 00:31:25 +0800

    QUADRANT

--- sample.js
+++ sample.js
@@ -1,5 +1,19 @@
-let device = ['realme', 'Vivo', 'apple', 'samsung'];
+let x = 25;
+let y = 15;
 
-for (let cellphone of device) {
-    console.log(device);
-}
+console.log('Coordinte x,y');
+
+if (x > 0 && y > 0) {
+    console.log('Quadrant 1, (+, +)');
+} else if (x < 0 && y > o) {
+    console.log('Quadrant 2, (-, +)');
+} else if (x < 0 && y < 0) {
+    console.log('Quadrant 3, (-, -)');
+} else if (x > 0 && y < 0) {
+    console.log('Quadrant 4, (+, -)');
+} else if (x != 0 && y == 0) {
+    console.log('X-AXIS');
+} else if (x == 0 && y != 0) {
+    console.log('Y-AXIS');
+} else (x == 0 && y == 0) 
+    console.log('ORIGIN');
