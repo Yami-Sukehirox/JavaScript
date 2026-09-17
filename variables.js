ebbf66f5b164895b6671e845611a964b283afaff
unknown <liancruspero1@gmail.com>
2026-09-17 09:04:18 +0800

    NEW

--- variables.js
+++ variables.js
@@ -370,4 +370,40 @@ function arr(num1, num2) {
 }
 
 console.log( arr(2, 3));
-    
+
+//=============================================
+function fizzBuzz(number) {
+    if (number % 3 === 0 && number % 5 === 0)
+        return 'FizzBuzz';
+    else if (number % 3 === 0)
+        return 'Fizz';
+    else if (number % 5 === 0)
+        return 'Buzz';
+    else
+        return number;
+}
+console.log( fizzBuzz (15) );
+
+//=====================================================
+const sub = {
+    mathematics: 99,
+    programming: 97,
+    algorithm: 90
+};
+
+const average = 
+    (sub.mathematics + sub.programming + sub.algorithm) / 3;
+
+console.log('Average: ' + average);
+
+function grades(average) {
+    if (average >= 90) 
+        return 'Status: Excellent';
+    else if (average >= 75) 
+        return 'Status: Passed';
+    else 
+        return 'Status: Failed';
+}
+
+console.log ( grades (average));
+//========================================================
